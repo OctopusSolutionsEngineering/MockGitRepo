9244d3fb8fb7bdc0edf23021e727a1fcd7014eab
Matthew Casperson <matthewcasperson@gmail.com>
2026-08-03 12:58:47 +1000

    Update sample repo

--- packtemplates.sh
+++ packtemplates.sh
@@ -4,7 +4,7 @@ set -e
 tar -cjf repotemplate.tar.bz2 repotemplate
 if [[ -f repotemplate.tar.bz2 ]]; then
     echo "repotemplate.tar.bz2 created successfully."
-    rm -rf repotemplate
+    #rm -rf repotemplate
 else
     echo "Failed to create repotemplate.tar.bz2."
     exit 1
